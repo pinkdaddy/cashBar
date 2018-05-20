bc14b911883e0c5266208889b0f55d70850dd708
John <easton3003@gmail.com>
2018-05-20 16:44:32 +0100

    feat(contact): add memo.cash profile

--- app/menus/contact.js
+++ app/menus/contact.js
@@ -3,6 +3,7 @@ const { shell } = require('electron'); // eslint-disable-line
 module.exports = {
   label: 'Contact',
   submenu: [
+    { label: 'Memo', click() { shell.openExternal('https://memo.cash/profile/12CLvMpDxyeTMkf2XE2LdvEAX3poShMy4n'); } },
     { label: 'Twitter', click() { shell.openExternal('https://twitter.com/johneas10'); } },
     { label: 'Github', click() { shell.openExternal('https://github.com/johneas10/cashBar'); } },
   ],
